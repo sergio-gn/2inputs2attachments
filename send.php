ab5d7bf9baa07333f115141fdfcf471adf013186
Sergio Garcia Neto <88346250+sergio-gn@users.noreply.github.com>
2023-04-12 15:13:21 +1000

    Update send.php

--- send.php
+++ send.php
@@ -7,15 +7,15 @@ require("PHPMailer-master/src/SMTP.php");
  $mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
  $mail->SMTPAuth = true; // authentication enabled
  $mail->SMTPSecure = 'tsl'; // secure transfer enabled REQUIRED for Gmail
- $mail->Host = "mail.meiaentradabrasil.com.br";
+ $mail->Host = "mail.examplehost.com";
  $mail->Port = 587; // or 587
  $mail->IsHTML(true);
- $mail->Username = "cadastro@meiaentradabrasil.com.br";
- $mail->Password = "Lesenechal@1234";
- $mail->SetFrom("cadastro@meiaentradabrasil.com.br");
+ $mail->Username = "exampleemail@example.com";
+ $mail->Password = "ExamplePassword123";
+ $mail->SetFrom("exampleemail@example.com");
  $mail->Subject = "Assunto da mensagem";
  $mail->Body = "Escreva o texto do email aqui";
- $mail->AddAddress("cadastro.meiaentradabrasil@gmail.com"); //cadastro@meiaentradabrasil.com.br
+ $mail->AddAddress("exampleemailadded@gmail.com");
  for ($i=0; $i < count($_FILES['file']['tmp_name']); $i++){
       $mail->addAttachment($_FILES['file']['tmp_name'][$i], $_FILES['file']['name'][$i]);
  }
